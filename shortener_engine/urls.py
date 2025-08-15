32baa1c47c0e7c0cbd921d6ef688f633a764d3e5
appyouz <dikpalkhatri7@gmail.com>
2025-08-15 17:47:47 +0545

    feat: add redirect url

--- shortener_engine/urls.py
+++ shortener_engine/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import URLView
+from .views import URLView,ShortUrlView
+from .import views
 
 urlpatterns = [
-    path('shorten/', URLView.as_view(), name='shorten_url'),
+    path('shorten/', views.URLView.as_view(), name='shorten_url'),
+    path('<str:short_key>/', views.ShortUrlView.as_view(), name='redirect'),
 ]
